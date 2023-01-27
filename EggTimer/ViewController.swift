8c6567b2f8784b7d2e61d8d0a9d977c365aa520c
Mark Raitman <markraitman@mail.ru>
2023-01-27 22:20:39 +0300

    Добавлен IBAction на кнопки, также сделана обработка результатов по нажатию на кнопки

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Constants
+    let softTime = 5
+    let mediumTime = 8
+    let hardTime = 12
 
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    //MARK: Buttons
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        
+        if hardness == "Soft" {
+            print(softTime)
+        } else if hardness == "Medium" {
+            print(mediumTime)
+        } else if hardness == "Hard" {
+            print(hardTime)
+        } else {
+            print("Error!")
+        }
+    }
+ }
 
-}
 
