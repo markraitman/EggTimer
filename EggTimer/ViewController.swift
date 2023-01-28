fa1a178907633fb87ae390e10848086bebdda48f
Mark Raitman <markraitman@mail.ru>
2023-01-28 21:32:31 +0300

    Добавлен словарь из значений свойства яйца и времени приготовления, также реализована функция таймера  обратного отсчёта с изменением главного лейбла

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,30 +10,46 @@ import UIKit
 class ViewController: UIViewController {
     
     //MARK: Constants
-    let softTime = 5
-    let mediumTime = 8
-    let hardTime = 12
-
+    let eggTimes = ["Soft" : 300, "Medium" : 450, "Hard" : 720]
+    
+    var secondsRemaining = 60
+    
+    var timer = Timer()
+    
+    //MARK: Outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    
     //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    //MARK: Buttons
     
+    //MARK: Actions
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
         let hardness = sender.currentTitle!
         
-        if hardness == "Soft" {
-            print(softTime)
-        } else if hardness == "Medium" {
-            print(mediumTime)
-        } else if hardness == "Hard" {
-            print(hardTime)
+        secondsRemaining = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+        titleLabel.text = "Nice choice! Wait about \(secondsRemaining / 60) min, please(="
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds")
+            secondsRemaining -= 1
         } else {
-            print("Error!")
+            timer.invalidate()
+            titleLabel.text = "Done!"
         }
     }
- }
+    
+    
+    
+}
 
 
